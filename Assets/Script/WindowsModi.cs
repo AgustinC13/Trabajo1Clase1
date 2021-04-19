f998ebdcf69754a2a534c1ea36054ca49ee14623
AgustinC13 <agustincannistraro76@hotmail.com>
2021-04-19 00:30:49 -0300

    Primer commit

---
+++ Assets/Script/WindowsModi.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class WindowsModi : EditorWindow{
+
+    Color color;
+
+   [MenuItem("Window/Colorizador")]
+   public static void ShowWindow ()
+    {
+        GetWindow<WindowsModi>("Colorizador");
+    }
+
+
+    void OnGUI ()
+    {
+        GUILayout.Label("Colorear objeto seleccionado", EditorStyles.boldLabel);
+
+       color = EditorGUILayout.ColorField("Color", color);
+
+        if (GUILayout.Button("Colorear"))
+        {
+            Colorize();
+        }
+    }
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+}
